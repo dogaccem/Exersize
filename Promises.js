ee625bd9cdc5a44146b7921ef472bb78ce88e587
dogaccem <dogacisoglu@gmail.com>
2022-01-24 15:14:52 +0300

    nodejs

--- Promises.js
+++ Promises.js
@@ -1,17 +1,30 @@
-var myPromises = new Promise(function(resolve, reject){
-    var result = true;
-    if(result){
-        resolve("success");
-    }
-    else{
-        reject("error");
-    }
-});
+// var myPromises = new Promise(function(resolve, reject){
+//     var result = true;
+//     if(result){
+//         resolve("success");
+//     }
+//     else{
+//         reject("error");
+//     }
+// });
 
-myPromises
-.then(data =>{
-    console.log(data);
-})
-.catch(err=>{
-    console.log(err);
-})
+// myPromises
+// .then(data =>{
+//     console.log(data);
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('foo');
+    }, 300);
+  });
+  
+  myPromise
+    .then(handleResolvedA, handleRejectedA)
+    .then(handleResolvedB, handleRejectedB)
+    .then(handleResolvedC, handleRejectedC)
+    .catch(handleRejectedAny);
+  
