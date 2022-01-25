efcd6ca83686aaf84ce5ac34f0c5a8f74f6d29fd
dogaccem <dogacisoglu@gmail.com>
2022-01-25 23:57:16 +0300

    ignore

--- main.js
+++ main.js
@@ -26,28 +26,4 @@
 
 // cities.forEach(getAllCb);
 
-//cities.forEach(function(name,index){console.log("name:",name);})
-
-// function myFunc(x,y,callback)
-// {
-//     console.log("1.satır");
-//     callback();
-//     console.log("3.satır");
-// }
-
-// function callbackFunc()
-// {
-//     console.log("2.satır");
-// }
-
-// myFunc(1,2,callbackFunc);
-
-var myFunc = (callback) => {
-    console.log("1.satır");
-    callback();
-    console.log("3.satır");
-}
-
-var callbackFunc = () =>{console.log("2.satır");}
-
-myFunc(callbackFunc);
+// cities.forEach(function(name,index){console.log("name:",name);})
